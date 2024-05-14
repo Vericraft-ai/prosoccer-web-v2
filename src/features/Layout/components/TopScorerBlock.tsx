cca6bcfacd3574430091153fae0f23147bc56fea
sleekyom <amdas72@gmail.com>
2024-05-15 02:20:17 +0300

    Refactor MatchTable and TopScorerBlock components

--- src/features/Layout/components/TopScorerBlock.tsx
+++ src/features/Layout/components/TopScorerBlock.tsx
@@ -23,30 +23,28 @@ export const TopScorerBlock = () => {
       <Table className="text-broly bg-vegeta rounded-xl mt-4">
         <TableBody className="">
           {topScorersData?.map((player) => (
-            <div className="flex flex-row w-full">
-              <TableRow
-                key={player.player_id}
-                className="border-none w-full my-3 p-3 flex justify-between items-center"
-              >
-                <TableCell className="font-medium text-center ">
-                  {player.player_id}
-                </TableCell>
-                <TableCell className="font-medium ">
-                  {<Gamepad2 size={24} />}
-                </TableCell>
-                <TableCell className="font-medium text-left ">
-                  <div className="flex flex-col">
-                    <span>{player.player_name}</span>
-                    <span>
-                      {player.team_name} | {player.matches_played}
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="text-right ">
-                  {player.goals_scored}
-                </TableCell>
-              </TableRow>
-            </div>
+            <TableRow
+              key={player.player_id}
+              className="border-nonemy-3 p-3 flex flex-row w-full justify-between items-center"
+            >
+              <TableCell className="font-medium text-center ">
+                {player.player_id}
+              </TableCell>
+              <TableCell className="font-medium ">
+                {<Gamepad2 size={24} />}
+              </TableCell>
+              <TableCell className="font-medium text-left ">
+                <div className="flex flex-col">
+                  <span>{player.player_name}</span>
+                  <span>
+                    {player.team_name} | {player.matches_played}
+                  </span>
+                </div>
+              </TableCell>
+              <TableCell className="text-right ">
+                {player.goals_scored}
+              </TableCell>
+            </TableRow>
           ))}
         </TableBody>
       </Table>
