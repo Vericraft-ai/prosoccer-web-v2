e0926135e37536d570f52dc29e0fc8213442bffa
sleekyom <amdas72@gmail.com>
2024-05-25 17:49:39 +0300

    refactor: Remove unused PitchCard component and update Squad page layout

--- src/features/Squad/components/PitchCard.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import FootballField from "@/assets/icons/Pitch.svg";
-
-export const PitchCard = () => {
-  return <div>PitchCard</div>;
-};
--- src/pages/squad/index.tsx
+++ src/pages/squad/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Header } from "@/features/Layout/components/Header";
 import { Wrapper } from "@/features/Layout/components/Wrapper";
+import { LineUp } from "@/features/Squad/components/LineUp";
 
 const squad = () => {
   return (
     <Wrapper>
       <Header>
-        <div className="text-broly text-lg">
-          The easiest way to start is to buy a bundle of players. So go to our
-          marketplace, explore the bundles and get your first squad!
+        <div className="w-full flex flex-col items-center justify-center">
+          <div className="text-broly text-lg text-center">
+            The easiest way to start is to buy a bundle of players. So go to our
+            marketplace, explore the bundles and get your first squad!
+          </div>
+          <LineUp />
         </div>
       </Header>
     </Wrapper>
