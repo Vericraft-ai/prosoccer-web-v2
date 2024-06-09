bcf24d908222e89ffcbb08bf05a86ea8050a7f02
tobianointing <adeyokunnuo@gmail.com>
2024-06-09 19:05:15 +0100

    fix: home/Header component

--- src/features/Layout/components/Header.tsx
+++ src/features/Layout/components/Header.tsx
@@ -7,12 +7,14 @@ import { LogoOnly } from "@/blocks/icons/LogoOnly";
 type HeaderProps = {
   search?: boolean;
   isConnected?: boolean;
+  children?: React.ReactNode;
 };
 
-export const Header = ({ search, isConnected }: HeaderProps) => {
+export const Header = ({ search, isConnected, children }: HeaderProps) => {
   return (
     <div className="flex w-full">
       <div className="flex flex-col-reverse lg:flex-row justify-between w-full items-center gap-10 lg:gap-0">
+        {children}
         {search && <SearchInput onSearch={(query) => console.log(query)} />}
         <div className="flex items-center gap-6 lg:gap-11 justify-between lg:justify-end lg:self-end w-full">
           <LogoOnly className="lg:hidden shrink-0" />
